371ebd76ac5eb87a47fea84dfa8f14c23942e8b3
John Washam <john@readyprompt.com>
2016-06-30 02:09:06 +0000

    Renamed entry

--- flash_cards.py
+++ flash_cards.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World, you ready to learn?'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
