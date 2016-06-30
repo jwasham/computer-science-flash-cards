4d616fad36bdacee7903a2eeeac41156d282c0e8
John Washam <john@readyprompt.com>
2016-06-29 17:20:15 -0700

    Updating test.

--- index.py
+++ index.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World, you ready to learn?'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
