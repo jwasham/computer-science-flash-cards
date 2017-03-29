5359c12ddc2e925fcbde4aa4cb6b6220028c2d20
bingqiang.lin <bingqiang.lin1@gmail.com>
2017-03-29 11:32:51 +0800

    Update flash_cards.py
    change flash_cards.py

--- flash_cards.py
+++ flash_cards.py
@@ -49,10 +49,10 @@ def close_db(error):
 
 # Uncomment and use this to initialize database, then comment it
 #   You can rerun it to pave the database and start over
-#@app.route('/initdb')
-#def initdb():
-#    init_db()
-#    return 'Initialized the database.'
+# @app.route('/initdb')
+# def initdb():
+#     init_db()
+#     return 'Initialized the database.'
 
 
 @app.route('/')
